8e490eb32debb7248047bc58cf130b2a3b0377ad
lostark13 <tushar9915@gmail.com>
2024-05-22 13:50:10 +0800

    RMX3371: parts: Initialize parts with per-app refresh rate
    Change-Id: I8ec82c63c8003f21ce4b969d2b173d4287906058

--- device.mk
+++ device.mk
@@ -119,6 +119,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    RealmeParts
+
 # Device ID attestation
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
