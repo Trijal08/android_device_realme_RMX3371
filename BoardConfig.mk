ae12374229be6d28ab92b033db15bdb8751926eb
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-07-22 16:31:03 -0400

    Unfreeze SPL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security
-VENDOR_SECURITY_PATCH := 2025-03-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
