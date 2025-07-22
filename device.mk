97101761c8c41e14e420100abbcc73c7d363a605
ryukftw <ryukv009@gmail.com>
2025-07-22 09:00:51 +0000

    RMX3371: Move OplusDolby to device-specific settings section
    - Consolidate audio packages for better organization
    
    Change-Id: Iedf09bd416b8b415a27f5f95434eef37aa08d382

--- device.mk
+++ device.mk
@@ -128,7 +128,8 @@ PRODUCT_COPY_FILES += \
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
-    DSPVolumeSynchronizer
+    DSPVolumeSynchronizer \
+    OplusDolby
 
 # Display
 PRODUCT_PACKAGES += \
@@ -141,10 +142,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-
-# Dolby
-PRODUCT_PACKAGES += \
-    OplusDolby
 
 # DRM
 PRODUCT_PACKAGES += \
