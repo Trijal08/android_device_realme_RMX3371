b24ee9a8337048eaded064a090781b5d96671dda
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-07-28 16:04:08 -0400

    Revert "parts: Fixup"
    This reverts commit 0c18e2606e4fa5f41b44018c8a3011860c3aef2f.

--- device.mk
+++ device.mk
@@ -134,10 +134,6 @@ PRODUCT_PACKAGES += \
     DSPVolumeSynchronizer \
     OplusDolby
 
-# Parts
-PRODUCT_PACKAGES += \
-    MyParts
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
